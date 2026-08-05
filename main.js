155c8d754128a9ab278d51c17b0992135cddd608
koninismacht-137 <koninismacht@gmail.com>
2026-08-05 19:11:41 +0200

    Implement showTodayContent and buildArchive functions
    Added functions to display today's content and build an archive.

--- main.js
+++ main.js
@@ -6,3 +6,22 @@ function showRandomContent() {
     document.getElementById("mood").innerText = item.mood;
     document.getElementById("text").innerText = item.text;
 }
+function showTodayContent() {
+    const index = new Date().getDate() % contents.length;
+    const item = contents[index];
+
+    document.getElementById("type").innerText = item.type;
+    document.getElementById("title").innerText = item.title;
+    document.getElementById("mood").innerText = item.mood;
+    document.getElementById("text").innerText = item.text;
+}
+function buildArchive() {
+    const archive = document.getElementById("archive");
+
+    contents.forEach((item, i) => {
+        const div = document.createElement("div");
+        div.className = "archive-item";
+        div.innerText = `${i+1}. ${item.title} (${item.type})`;
+        archive.appendChild(div);
+    });
+}
