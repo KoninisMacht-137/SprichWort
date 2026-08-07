c604c4c02247d3d139a4eef3a7f4c0d334c101d1
koninismacht-137 <koninismacht@gmail.com>
2026-08-07 13:07:36 +0200

    Remove buildArchive function and its call
    Removed the buildArchive function and its invocation.

--- main.js
+++ main.js
@@ -15,23 +15,3 @@ function showTodayContent() {
     document.getElementById("mood").innerText = item.mood;
     document.getElementById("text").innerText = item.text;
 }
-function buildArchive() {
-    const archiveList = document.querySelector(".archive-list");
-    archiveList.innerHTML = ""; // leeren
-
-    contents.forEach(item => {
-        const div = document.createElement("div");
-        div.className = "archive-item";
-
-        div.innerHTML = `
-            <span class="archive-emoji">${item.emoji}</span>
-            <span class="archive-title">${item.title}</span>
-            <span class="archive-type" style="opacity:0.6;">(${item.type})</span>
-        `;
-
-        archiveList.appendChild(div);
-    });
-}
-
-// Archiv direkt beim Laden erzeugen
-buildArchive();
